51f5908d73d6989cb18b5778b4b1996c54563269
Kivanc Muslu <kivancm@microsoft.com>
2020-09-23 15:04:19 -0700

    Introduce IWebRequestStatsTracker
    This is one of the changes to bring StatsTracker from AzureDevOps collectos so that it can be also used with the GitHub collectors and becomes part of the core library.

---
+++ Core.Collectors/Collector/IWebRequestStatsTracker.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.CloudMine.Core.Collectors.Collector
+{
+    /// <summary>
+    /// Abstracts the capability of tracking the number of successful and failed web requests such that an HttpClient can be used in a StatsTracker.
+    /// </summary>
+    public interface IWebRequestStatsTracker
+    {
+        int SuccessfulRequestCount { get; }
+        int FailedRequestCount { get; }
+    }
+}
