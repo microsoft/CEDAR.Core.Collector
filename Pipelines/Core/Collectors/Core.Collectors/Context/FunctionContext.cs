307ef4c42534d6e37f892afc59b0febddc085aa5
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2020-10-14 13:10:22 -0700

    Add SliceDate to FunctionContext (#12)
    Also move, context-related writing functionality to the function context writer.

--- Pipelines/Core/Collectors/Core.Collectors/Context/FunctionContext.cs
+++ Pipelines/Core/Collectors/Core.Collectors/Context/FunctionContext.cs
@@ -20,5 +20,10 @@ namespace Microsoft.CloudMine.Core.Collectors.Context
         /// In case a function triggers another one, used to keep relation between multiple invocations.
         /// </summary>
         public string CorrelationId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// For functions that are running on a schedule, represents the time the collection should have started.
+        /// </summary>
+        public DateTime SliceDate { get; set; } = DateTime.MinValue;
     }
 }
