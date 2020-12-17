d7aa107336de92fb59137ee3ac3872d2163dc588
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2020-12-16 17:39:04 -0800

    Remove non-generic FunctionContextWriter (#25)
    This is no longer needed. Just cleaning up.

--- Pipelines/Core/Collectors/Core.Collectors/Context/FunctionContextWriter.cs
+++ Pipelines/Core/Collectors/Core.Collectors/Context/FunctionContextWriter.cs
@@ -20,19 +20,4 @@ namespace Microsoft.CloudMine.Core.Collectors.Context
             }
         }
     }
-
-    public class FunctionContextWriter : ContextWriter<FunctionContext>
-    {
-        public override void AugmentMetadata(JObject metadata, FunctionContext functionContext)
-        {
-            metadata.Add("FunctionStartDate", functionContext.FunctionStartDate);
-            metadata.Add("SessionId", functionContext.SessionId);
-            metadata.Add("CollectorType", functionContext.CollectorType.ToString());
-
-            if (functionContext.SliceDate != DateTime.MinValue)
-            {
-                metadata.Add("SliceDate", functionContext.SliceDate);
-            }
-        }
-    }
 }
