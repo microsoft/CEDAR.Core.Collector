569f37479226cad720308008aea396571155e899
Myles McLeroy <mylesmcleroy@microsoft.com>
2021-01-22 12:04:58 -0600

    Move ADLS app ingestion details from local to global settings

--- Core.Collectors/Web/AdlsClientWrapper.cs
+++ Core.Collectors/Web/AdlsClientWrapper.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 using Microsoft.Azure.DataLake.Store;
+using Microsoft.CloudMine.Core.Collectors.Error;
+using Microsoft.CloudMine.Core.Collectors.Utility;
 using Microsoft.Rest;
 using Microsoft.Rest.Azure.Authentication;
+using Newtonsoft.Json.Linq;
 using System;
 
 namespace Microsoft.CloudMine.Core.Collectors.Web
@@ -21,17 +24,24 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
 
         public AdlsClient AdlsClient { get; private set; }
 
-        public AdlsClientWrapper()
+        public AdlsClientWrapper(string settings)
         {
-            string clientId = Environment.GetEnvironmentVariable("AdlsIngestionApplicationId");
-            string secretKey = Environment.GetEnvironmentVariable("AdlsIngestionApplicationSecret");
-
-            if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(secretKey))
+            JObject config = JObject.Parse(settings);
+            JToken clientIdToken = config.SelectToken("AdlsIngestionApplicationId");
+            JToken secretKeyEnvironmentVariableToken = config.SelectToken("AdlsIngestionApplicationSecretEnvironmentVariable");
+            if (JTokenUtility.IsNullOrWhiteSpace(clientIdToken) || JTokenUtility.IsNullOrWhiteSpace(secretKeyEnvironmentVariableToken))
             {
                 // Don't fail loading the function app environment if these variables are not provided. Instead don't initialize the ADLS client.
                 // This way, we permit functions that don't depend on the ADLS client to still run without configuring the ADLS client.
                 // Once the function loads, we will also log the fact that ADLS client is not initialized separately.
                 return;
+            }
+
+            string clientId = clientIdToken.Value<string>();
+            string secretKey = Environment.GetEnvironmentVariable(secretKeyEnvironmentVariableToken.Value<string>());
+            if (string.IsNullOrWhiteSpace(secretKey))
+            {
+                throw new FatalTerminalException($"For token '{secretKeyEnvironmentVariableToken}', local.settings.json must provide an ADLS secret key.");
             }
 
             ActiveDirectoryServiceSettings serviceSettings = ActiveDirectoryServiceSettings.Azure;
