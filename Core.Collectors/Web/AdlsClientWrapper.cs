e13fb450a5d56e59109b45a653c27f0939eb46fa
Myles McLeroy <76016514+myles-mcleroy@users.noreply.github.com>
2021-04-12 16:17:56 -0500

    Move ADLS ingestion app details from local to global settings (#42)
    This PR moves the ADLS ingestion application details from local.settings.json to Settings.json. 
    
    To do this, I pass the global settings JSON string to AdlsClientWrapper and parse it for two new keys `AdlsIngestionApplicationId` and `AdlsIngestionApplicationSecretEnvironmentVariable`. The former is moved directly from local settings, the latter contains the environment variable name of the ADLS secret. The global settings string is accessed from builder services (changes in `ServiceStartup.Configure()` in CEDAR/GitHub and ADO will be made to add the string to builder services).
    
    I also added a utility class `JTokenUtility` that allows me to perform checks on JTokens just like `string.IsNullOrEmpty()` and `string.IsNullOrWhiteSpace()`. This is great because it cleans up the checks in AdlsClientWrapper() (number of checks needed reduced from 4 -> 2) and will allow me to clean up other code in the collectors in future PRs.

--- Core.Collectors/Web/AdlsClientWrapper.cs
+++ Core.Collectors/Web/AdlsClientWrapper.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 using Microsoft.Azure.DataLake.Store;
+using Microsoft.CloudMine.Core.Collectors.Error;
+using Microsoft.CloudMine.Core.Collectors.Utility;
 using Microsoft.Rest;
 using Microsoft.Rest.Azure.Authentication;
+using Newtonsoft.Json.Linq;
 using System;
 
 namespace Microsoft.CloudMine.Core.Collectors.Web
@@ -19,7 +22,57 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
 
         public AdlsClient AdlsClient { get; private set; }
 
+        // This is the old AdlsClientWrapper API.
+        // The old API will be deprecated soon, but to protect our jobs from failing, we need to support both the old API and new API.
         public AdlsClientWrapper()
+            : this(settings: null)
+        {
+        }
+
+        public AdlsClientWrapper(string settings)
+        {
+            if (settings == null)
+            {
+                // Some products that use ADLS Client does not have config-based settings, e.g., RecordOutputService. For these products, also support having a 'null' setting and loading config from environment variables (as before).
+                this.LoadSettingsFromEnvironmentVariables();
+                return;
+            }
+
+            JObject config = JObject.Parse(settings);
+            JToken adlsClientToken = config.SelectToken("AzureDataLakeStorage");
+            if (adlsClientToken == null)
+            {
+                // Don't fail loading the function app environment if these variables are not provided. Instead don't initialize the ADLS client.
+                // This way, we permit functions that don't depend on the ADLS client to still run without configuring the ADLS client.
+                // Once the function loads, we will also log the fact that ADLS client is not initialized separately.
+                return;
+            }
+
+            JToken adlsAccountToken = adlsClientToken.SelectToken("Account");
+            JToken adlsTenantIdToken = adlsClientToken.SelectToken("TenantId");
+            JToken adlsIngestionApplicationIdToken = adlsClientToken.SelectToken("IngestionApplicationId");
+            JToken adlsIngestionApplicationSecretEnvironmentVariableToken = adlsClientToken.SelectToken("IngestionApplicationSecretEnvironmentVariable");
+            if (adlsIngestionApplicationIdToken.IsNullOrWhiteSpace() || adlsIngestionApplicationSecretEnvironmentVariableToken.IsNullOrWhiteSpace() || adlsAccountToken.IsNullOrWhiteSpace() || adlsTenantIdToken.IsNullOrWhiteSpace())
+            {
+                // Don't fail loading the function app environment if these variables are not provided. Instead don't initialize the ADLS client.
+                // This way, we permit functions that don't depend on the ADLS client to still run without configuring the ADLS client.
+                // Once the function loads, we will also log the fact that ADLS client is not initialized separately.
+                return;
+            }
+
+            string adlsAccount = adlsAccountToken.Value<string>();
+            string adlsTenantId = adlsTenantIdToken.Value<string>();
+            string adlsIngestionApplicationId = adlsIngestionApplicationIdToken.Value<string>();
+            string adlsIngestionApplicationSecret = Environment.GetEnvironmentVariable(adlsIngestionApplicationSecretEnvironmentVariableToken.Value<string>());
+            if (string.IsNullOrWhiteSpace(adlsIngestionApplicationSecret))
+            {
+                throw new FatalTerminalException($"For token '{adlsIngestionApplicationSecretEnvironmentVariableToken}', local.settings.json must provide an ADLS Ingestion Application Secret.");
+            }
+
+            this.InitializeAdlsClient(adlsAccount, adlsTenantId, adlsIngestionApplicationId, adlsIngestionApplicationSecret);
+        }
+
+        private void LoadSettingsFromEnvironmentVariables()
         {
             string adlsAccount = Environment.GetEnvironmentVariable("AdlsAccount");
             if (string.IsNullOrWhiteSpace(adlsAccount))
@@ -33,10 +86,10 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
                 adlsTenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
             }
 
-            string clientId = Environment.GetEnvironmentVariable("AdlsIngestionApplicationId");
-            string secretKey = Environment.GetEnvironmentVariable("AdlsIngestionApplicationSecret");
+            string adlsIngestionApplicationId = Environment.GetEnvironmentVariable("AdlsIngestionApplicationId");
+            string adlsIngestionApplicationSecret = Environment.GetEnvironmentVariable("AdlsIngestionApplicationSecret");
 
-            if (string.IsNullOrWhiteSpace(clientId) || string.IsNullOrWhiteSpace(secretKey))
+            if (string.IsNullOrWhiteSpace(adlsIngestionApplicationId) || string.IsNullOrWhiteSpace(adlsIngestionApplicationSecret))
             {
                 // Don't fail loading the function app environment if these variables are not provided. Instead don't initialize the ADLS client.
                 // This way, we permit functions that don't depend on the ADLS client to still run without configuring the ADLS client.
@@ -44,6 +97,11 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
                 return;
             }
 
+            this.InitializeAdlsClient(adlsAccount, adlsTenantId, adlsIngestionApplicationId, adlsIngestionApplicationSecret);
+        }
+
+        private void InitializeAdlsClient(string adlsAccount, string adlsTenantId, string clientId, string secretKey)
+        {
             ActiveDirectoryServiceSettings serviceSettings = ActiveDirectoryServiceSettings.Azure;
             serviceSettings.TokenAudience = AdlTokenAudience;
             ServiceClientCredentials adlCreds = ApplicationTokenProvider.LoginSilentAsync(adlsTenantId, clientId, secretKey, serviceSettings).GetAwaiter().GetResult();
