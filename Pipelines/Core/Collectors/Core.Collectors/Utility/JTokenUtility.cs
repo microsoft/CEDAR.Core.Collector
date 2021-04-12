f4ef995d22f8a4bc3152452dab76584409fd7369
Myles McLeroy <76016514+myles-mcleroy@users.noreply.github.com>
2021-04-12 16:17:56 -0500

    Move ADLS ingestion app details from local to global settings (#42)
    This PR moves the ADLS ingestion application details from local.settings.json to Settings.json. 
    
    To do this, I pass the global settings JSON string to AdlsClientWrapper and parse it for two new keys `AdlsIngestionApplicationId` and `AdlsIngestionApplicationSecretEnvironmentVariable`. The former is moved directly from local settings, the latter contains the environment variable name of the ADLS secret. The global settings string is accessed from builder services (changes in `ServiceStartup.Configure()` in CEDAR/GitHub and ADO will be made to add the string to builder services).
    
    I also added a utility class `JTokenUtility` that allows me to perform checks on JTokens just like `string.IsNullOrEmpty()` and `string.IsNullOrWhiteSpace()`. This is great because it cleans up the checks in AdlsClientWrapper() (number of checks needed reduced from 4 -> 2) and will allow me to clean up other code in the collectors in future PRs.

---
+++ Pipelines/Core/Collectors/Core.Collectors/Utility/JTokenUtility.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.CloudMine.Core.Collectors.Utility
+{
+    public static class JTokenUtility
+    {
+        private static bool IsNull(JToken token)
+        {
+            return token == null;
+        }
+
+        private static bool IsEmpty(JToken token)
+        {
+            return (token.Type == JTokenType.Array && !token.HasValues) ||
+                   (token.Type == JTokenType.Object && !token.HasValues) ||
+                   (token.Type == JTokenType.String && token.ToString() == string.Empty);
+        }
+
+        private static bool IsWhiteSpace(JToken token)
+        {
+            return token.Value<string>().All(char.IsWhiteSpace);
+        }
+
+        // This method is functionally the same as string.IsNullOrEmpty(), but it works with a JToken contains any type. 
+        // This reduces the number of checks you need to make from 4 to 1 if you want to determine if the value of a JToken containing an unknown type is null or empty.
+        public static bool IsNullOrEmpty(this JToken token)
+        {
+            return IsNull(token) || IsEmpty(token);
+        }
+
+        // This method is functionally the same as string.IsNullOrWhiteSpace(), but it works with a JToken that contains any type.
+        // This reduces the number of checks you need to make from 5 to 1 if you want to determine if the value of a JToken containing an unknown type is null, empty, or whitespace.
+        public static bool IsNullOrWhiteSpace(this JToken token)
+        {
+            return IsNull(token) || IsEmpty(token) || IsWhiteSpace(token);
+        }
+    }
+}
