1be02e67c594ee1b288eb3ca600cc7404c19b1df
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2021-04-15 15:43:15 -0700

    Make HttpResponseSignature properties read-only fields. (#50)

--- Core.Collectors/Web/HttpResponseSignature.cs
+++ Core.Collectors/Web/HttpResponseSignature.cs
@@ -10,31 +10,31 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
 {
     public class HttpResponseSignature
     {
-        public HttpStatusCode StatusCode { get; }
-        public Regex ResponseMessageRegex { get; }
-        public Func<JObject, bool> Matcher { get; }
+        private readonly HttpStatusCode statusCode;
+        private readonly Regex responseMessageRegex;
+        private readonly Func<JObject, bool> matcher;
 
         public HttpResponseSignature(HttpStatusCode statusCode, string responseMessageRegex)
         {
-            this.StatusCode = statusCode;
-            this.ResponseMessageRegex = new Regex($"^{responseMessageRegex}$", RegexOptions.Compiled | RegexOptions.Multiline);
+            this.statusCode = statusCode;
+            this.responseMessageRegex = new Regex($"^{responseMessageRegex}$", RegexOptions.Compiled | RegexOptions.Multiline);
         }
 
         public HttpResponseSignature(HttpStatusCode statusCode, Func<JObject, bool> matcher)
         {
-            this.StatusCode = statusCode;
-            this.Matcher = matcher;
+            this.statusCode = statusCode;
+            this.matcher = matcher;
         }
 
         public bool Matches(HttpStatusCode statusCode, JObject responseContent, string responseMessagePath = "$.message")
         {
-            if (this.Matcher != null)
+            if (this.matcher != null)
             {
-                return this.StatusCode == statusCode && this.Matcher.Invoke(responseContent);
+                return this.statusCode == statusCode && this.matcher.Invoke(responseContent);
             }
 
             string responseMessage = responseContent.SelectToken(responseMessagePath).Value<string>();
-            return this.StatusCode == statusCode && this.ResponseMessageRegex.IsMatch(responseMessage);
+            return this.statusCode == statusCode && this.responseMessageRegex.IsMatch(responseMessage);
         }
     }
 }
