db510c71ecac5a8a1a9720e93dd172f9a0636a7f
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2021-04-15 15:07:50 -0700

    Clean-up API changes in HttpResponseSignature. (#49)

--- Core.Collectors/Web/HttpResponseSignature.cs
+++ Core.Collectors/Web/HttpResponseSignature.cs
@@ -10,27 +10,14 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
 {
     public class HttpResponseSignature
     {
-        public HttpStatusCode statusCode;
-        public HttpStatusCode StatusCode 
-        {
-            get
-            {
-                return this.statusCode;
-            }
-            set
-            {
-                this.statusCode = value;
-            }
-        }
+        public HttpStatusCode StatusCode { get; }
         public Regex ResponseMessageRegex { get; }
-        public string ResponseMessagePath { get; }
         public Func<JObject, bool> Matcher { get; }
 
-        public HttpResponseSignature(HttpStatusCode statusCode, string responseMessageRegex, string responseMessagePath = "$.message")
+        public HttpResponseSignature(HttpStatusCode statusCode, string responseMessageRegex)
         {
             this.StatusCode = statusCode;
             this.ResponseMessageRegex = new Regex($"^{responseMessageRegex}$", RegexOptions.Compiled | RegexOptions.Multiline);
-            this.ResponseMessagePath = responseMessagePath;
         }
 
         public HttpResponseSignature(HttpStatusCode statusCode, Func<JObject, bool> matcher)
@@ -39,20 +26,15 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
             this.Matcher = matcher;
         }
 
-        public bool Matches(HttpStatusCode statusCode, string responseMessage)
-        {
-            return this.StatusCode == statusCode && this.ResponseMessageRegex.IsMatch(responseMessage);
-        }
-
-        public bool Matches(HttpStatusCode statusCode, JObject responseContent)
+        public bool Matches(HttpStatusCode statusCode, JObject responseContent, string responseMessagePath = "$.message")
         {
             if (this.Matcher != null)
             {
                 return this.StatusCode == statusCode && this.Matcher.Invoke(responseContent);
             }
 
-            string responseMessage = responseContent.SelectToken("$.message").Value<string>();
-            return this.Matches(this.StatusCode, responseMessage);
+            string responseMessage = responseContent.SelectToken(responseMessagePath).Value<string>();
+            return this.StatusCode == statusCode && this.ResponseMessageRegex.IsMatch(responseMessage);
         }
     }
 }
