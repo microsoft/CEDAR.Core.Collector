810f92acb1fc6c629e838004adf36f92f1f162e2
Kivanc Muslu <kivancm@microsoft.com>
2021-04-23 10:59:19 -0700

    AsyncTaskExecutor
    Adds the ability to execute tasks in parallel (for tasks that are void)

---
+++ Core.Collectors/Utility/AsyncTaskExecutor.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.CloudMine.Core.Collectors.Utility
+{
+    public class AsyncTaskExecutor
+    {
+        private readonly int maxBatchSize;
+        private readonly List<Task> tasks;
+
+        public AsyncTaskExecutor(int maxBatchSize)
+        {
+            this.maxBatchSize = maxBatchSize;
+        }
+
+        public void Add(Task task)
+        {
+            this.tasks.Add(task);
+        }
+
+        public async Task WaitIfNeededAsync()
+        {
+            if (tasks.Count >= maxBatchSize)
+            {
+                await Task.WhenAll(tasks).ConfigureAwait(false);
+                tasks.Clear();
+            }
+        }
+    }
+}
