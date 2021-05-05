5ae938a9996104c211b11fd4dcc4bc6d8da7dace
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2021-05-04 23:47:33 -0700

    Extend AdlsBulkRecordWriter with multiple ingestion (AdlsConfig) capabilities. (#66)

---
+++ Core.Collectors/Web/AdlsClientWrapper2.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.DataLake.Store;
+using System;
+
+namespace Microsoft.CloudMine.Core.Collectors.Web
+{
+    public class AdlsClientWrapper2 : IAdlsClient
+    {
+        public static readonly Uri AdlTokenAudience = new Uri(@"https://datalake.azure.net/");
+
+        public AdlsClient AdlsClient { get; private set; }
+
+        public AdlsClientWrapper2()
+        {
+            string adlsAccount = Environment.GetEnvironmentVariable("AdlsAccount2");
+            string adlsTenantId = Environment.GetEnvironmentVariable("AdlsTenantId2");
+            string adlsIngestionApplicationId = Environment.GetEnvironmentVariable("AdlsIngestionApplicationId2");
+            string adlsIngestionApplicationSecret = Environment.GetEnvironmentVariable("AdlsIngestionApplicationSecret2");
+
+            if (string.IsNullOrWhiteSpace(adlsIngestionApplicationId) || string.IsNullOrWhiteSpace(adlsIngestionApplicationSecret) || string.IsNullOrWhiteSpace(adlsAccount) || string.IsNullOrWhiteSpace(adlsTenantId))
+            {
+                // Don't fail loading the function app environment if these variables are not provided. Instead don't initialize the ADLS client.
+                // This way, we permit functions that don't depend on the ADLS client to still run without configuring the ADLS client.
+                // Once the function loads, we will also log the fact that ADLS client is not initialized separately.
+                return;
+            }
+
+            this.AdlsClient = AdlsClientWrapper.InitializeAdlsClient(adlsAccount, adlsTenantId, adlsIngestionApplicationId, adlsIngestionApplicationSecret);
+        }
+    }
+}
