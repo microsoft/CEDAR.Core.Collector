f7c3b41ee78004ca74af4be0d556ffaa2a8448ef
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2021-05-04 23:47:33 -0700

    Extend AdlsBulkRecordWriter with multiple ingestion (AdlsConfig) capabilities. (#66)

--- Pipelines/Core/Collectors/Core.Collectors/IO/AdlsBulkRecordWriter.cs
+++ Pipelines/Core/Collectors/Core.Collectors/IO/AdlsBulkRecordWriter.cs
@@ -14,6 +14,20 @@ using System.Collections.Generic;
 
 namespace Microsoft.CloudMine.Core.Collectors.IO
 {
+    public class AdlsConfig
+    {
+        public AdlsClient AdlsClient { get; }
+        public string AdlsRoot { get; }
+        public string Version { get; }
+
+        public AdlsConfig(AdlsClient adlsClient, string adlsRoot, string version)
+        {
+            this.AdlsClient = adlsClient;
+            this.AdlsRoot = adlsRoot;
+            this.Version = version;
+        }
+    }
+
     public class AdlsBulkRecordWriter<T> : RecordWriterCore<T> where T : FunctionContext
     {
         private const long FileSizeLimit = 1024 * 1024 * 512; // 512 MB.
@@ -22,15 +36,14 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
         private readonly static TimeSpan MaxUploadDelay = TimeSpan.FromMinutes(10);
 
         private readonly string uniqueId;
-        private readonly string adlsRoot;
-        private readonly string version;
-        private readonly AdlsClient adlsClient;
+        private readonly List<AdlsConfig> adlsConfigs;
 
         private string localRoot;
 
         private string currentSuffix;
         private string currentLocalPath;
 
+        // Keeping this constructor for backwards compatibility for now.
         public AdlsBulkRecordWriter(AdlsClient adlsClient,
                                     string identifier,
                                     ITelemetryClient telemetryClient,
@@ -38,11 +51,18 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
                                     ContextWriter<T> contextWriter,
                                     string root,
                                     string version)
+            : this(adlsConfigs: new List<AdlsConfig>() { new AdlsConfig(adlsClient, root, version) }, identifier, telemetryClient, functionContext, contextWriter)
+        {
+        }
+
+        public AdlsBulkRecordWriter(List<AdlsConfig> adlsConfigs,
+                                    string identifier,
+                                    ITelemetryClient telemetryClient,
+                                    T functionContext,
+                                    ContextWriter<T> contextWriter)
             : base(identifier, telemetryClient, functionContext, contextWriter, RecordSizeLimit, FileSizeLimit, source: RecordWriterSource.AzureDataLake)
         {
-            this.adlsClient = adlsClient;
-            this.adlsRoot = root;
-            this.version = version;
+            this.adlsConfigs = adlsConfigs;
             this.uniqueId = functionContext.SessionId;
             this.currentSuffix = null;
         }
@@ -65,8 +85,13 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
             return Task.FromResult(result);
         }
 
-        protected override Task NotifyCurrentOutputAsync()
+        protected override async Task NotifyCurrentOutputAsync()
         {
+            if (this.adlsConfigs.Count == 0)
+            {
+                throw new FatalTerminalException("No ADLS Config found for upload.");
+            }
+
             // Assume that upload will take at most 10 minutes.
             DateTime dateTimeSignature = DateTime.UtcNow + MaxUploadDelay;
             string fileName = $"{this.GetOutputPathPrefix(dateTimeSignature)}{this.currentSuffix}.json";
@@ -93,46 +118,25 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
                 }
             }
 
-            Stopwatch uploadTimer = Stopwatch.StartNew();
-            string adlsDirectory = $"{this.adlsRoot}/{this.version}";
+            List<Task<string>> uploadTasks = new List<Task<string>>();
+            foreach (AdlsConfig adlsConfig in this.adlsConfigs)
+            {
+                Task<string> uploadTask = Task<string>.Factory.StartNew(() => BulkUploadToAdlsConfig(finalOutputPath, adlsConfig));
+                uploadTasks.Add(uploadTask);
+            }
+
             try
             {
-                TransferStatus status = this.adlsClient.BulkUpload(this.localRoot, adlsDirectory);
-                bool retried = false;
-                if (status.EntriesFailed.Count != 0)
+                IEnumerable<string> adlsDirectories = await Task.WhenAll<string>(uploadTasks).ConfigureAwait(false);
+                foreach (string adlsDirectory in adlsDirectories)
                 {
-                    retried = true;
-                    // Retry once.
-                    status = this.adlsClient.BulkUpload(this.localRoot, adlsDirectory, shouldOverwrite: IfExists.Fail);
-                    if (status.EntriesFailed.Count != 0)
-                    {
-                        foreach (SingleEntryTransferStatus failedTransferStatus in status.EntriesFailed)
-                        {
-                            Dictionary<string, string> transferStatusProperties = new Dictionary<string, string>()
-                            {
-                                { "EntryName", failedTransferStatus.EntryName },
-                                { "EntrySize", failedTransferStatus.EntrySize.ToString() },
-                                { "TransferErrors", failedTransferStatus.Errors },
-                                { "TransferStatus", failedTransferStatus.Status.ToString() },
-                                { "TransferType", failedTransferStatus.Type.ToString() },
-                            };
-                            this.TelemetryClient.TrackEvent("FailedTransferStatus", transferStatusProperties);
-                        }
-                        throw new FatalException($"Cannot bulk upload '{finalOutputPath}'.");
-                    }
+                    string finalAdlsOutputPath = finalOutputPath.Replace($"{this.localRoot}\\", $"{adlsDirectory}/");
+                    this.AddOutputPath(finalAdlsOutputPath);
                 }
-
-                uploadTimer.Stop();
-                TimeSpan uploadDuration = uploadTimer.Elapsed;
-
-                Dictionary<string, string> properties = new Dictionary<string, string>()
-                {
-                    { "Duration", uploadDuration.ToString() },
-                    { "Retried", retried.ToString() },
-                    { "SizeBytes", this.SizeInBytes.ToString() },
-                    { "LocalPath", finalOutputPath },
-                };
-                this.TelemetryClient.TrackEvent("AdlsUploadStats", properties);
+            }
+            catch (Exception exception)
+            {
+                this.TelemetryClient.TrackException(exception, "ADLS Bulk Record Writer: upload failed.");
             }
             finally
             {
@@ -155,11 +159,51 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
                     }
                 }
             }
+        }
 
-            string finalAdlsOutputPath = finalOutputPath.Replace($"{this.localRoot}\\", $"{adlsDirectory}/");
-            this.AddOutputPath(finalAdlsOutputPath);
+        private string BulkUploadToAdlsConfig(string finalOutputPath, AdlsConfig adlsConfig)
+        {
+            Stopwatch uploadTimer = Stopwatch.StartNew();
+            string adlsDirectory = $"{adlsConfig.AdlsRoot}/{adlsConfig.Version}";
 
-            return Task.CompletedTask;
+            TransferStatus status = adlsConfig.AdlsClient.BulkUpload(this.localRoot, adlsDirectory);
+            bool retried = false;
+            if (status.EntriesFailed.Count != 0)
+            {
+                retried = true;
+                // Retry once.
+                status = adlsConfig.AdlsClient.BulkUpload(this.localRoot, adlsDirectory, shouldOverwrite: IfExists.Fail);
+                if (status.EntriesFailed.Count != 0)
+                {
+                    foreach (SingleEntryTransferStatus failedTransferStatus in status.EntriesFailed)
+                    {
+                        Dictionary<string, string> transferStatusProperties = new Dictionary<string, string>()
+                        {
+                            { "EntryName", failedTransferStatus.EntryName },
+                            { "EntrySize", failedTransferStatus.EntrySize.ToString() },
+                            { "TransferErrors", failedTransferStatus.Errors },
+                            { "TransferStatus", failedTransferStatus.Status.ToString() },
+                            { "TransferType", failedTransferStatus.Type.ToString() },
+                        };
+                        this.TelemetryClient.TrackEvent("FailedTransferStatus", transferStatusProperties);
+                    }
+                    throw new FatalException($"Cannot bulk upload '{finalOutputPath}'.");
+                }
+            }
+
+            uploadTimer.Stop();
+            TimeSpan uploadDuration = uploadTimer.Elapsed;
+
+            Dictionary<string, string> properties = new Dictionary<string, string>()
+            {
+                { "Duration", uploadDuration.ToString() },
+                { "Retried", retried.ToString() },
+                { "SizeBytes", this.SizeInBytes.ToString() },
+                { "LocalPath", finalOutputPath },
+            };
+            this.TelemetryClient.TrackEvent("AdlsUploadStats", properties);
+
+            return adlsDirectory;
         }
 
         public override async Task FinalizeAsync()
