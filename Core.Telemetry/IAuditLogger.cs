6aa694f00cff25f01cd22ab5b9de2c2b78ad6733
luke gostling <lukegostling@microsoft.com>
2022-02-09 14:49:10 -0800

    added core.telemetry project

---
+++ Core.Telemetry/IAuditLogger.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using Microsoft.Cloud.InstrumentationFramework;
+
+namespace Microsoft.CloudMine.Core.Telemetry
+{
+    public interface IAuditLogger
+    {
+        void Initialize(string tenantIdentity, string roleIdentity);
+
+        void LogApplicationAuditEvent(ITelemetryClient telemetryClient, AuditMandatoryProperties auditMandatoryProperties, AuditOptionalProperties auditOptionalProperties = null);
+
+        void LogTokenGenerationAuditEvent(ITelemetryClient telemetryClient, OperationResult operationResult, TargetResource[] targetResources, CallerIdentity[] callerIdentities, AuditOptionalProperties auditOptionalProperties = null);
+
+        void LogCertificateFetchAuditEvent(ITelemetryClient telemetryClient, OperationResult operationResult, TargetResource[] targetResources, CallerIdentity[] callerIdentities, AuditOptionalProperties auditOptionalProperties = null);
+    }
+}
