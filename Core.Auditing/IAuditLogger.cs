01aaac2a150db77244ca2af45e65aa60a265615c
luke gostling <lukegostling@microsoft.com>
2022-02-09 13:27:57 -0800

    ,ove auditing to GitHub

---
+++ Core.Auditing/IAuditLogger.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using Microsoft.Cloud.InstrumentationFramework;
+using Microsoft.CloudMine.Core.Collectors.Telemetry;
+
+namespace Microsoft.CloudMine.Core.Auditing
+{
+    public interface IAuditLogger
+    {
+        void Initialize(string tenantIdentity, string roleIdentity);
+
+        void LogApplicationAuditEvent(ITelemetryClient telemetryClient, AuditMandatoryProperties auditMandatoryProperties, AuditOptionalProperties auditOptionalProperties = null);
+
+        void LogTokenGenerationAuditEvent(ITelemetryClient telemetryClient, OperationResult operationResult, TargetResource[] targetResources, CallerIdentity[] callerIdentities, AuditOptionalProperties auditOptionalProperties = null);
+
+        void LogCertificateFetchAuditEvent(ITelemetryClient telemetryClient, OperationResult operationResult, TargetResource[] targetResources, CallerIdentity[] callerIdentities, AuditOptionalProperties auditOptionalProperties = null);
+    }
+}
