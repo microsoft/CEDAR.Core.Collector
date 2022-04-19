51a73fceea88c6be0315f685960e0c62f0753c51
luke gostling <lukegostling@microsoft.com>
2022-04-19 13:48:09 -0700

    update package Microsoft.Extentions.Logging to version 3 to allow OpenTelemetry

--- Core.Telemetry/OpenTelemetryClient.cs
+++ Core.Telemetry/OpenTelemetryClient.cs
@@ -2,10 +2,11 @@
 using OpenTelemetry.Logs;
 using OpenTelemetry.Exporter.Geneva;
 using System.Collections.Generic;
+using System;
 
 namespace Microsoft.CloudMine.Core.Telemetry
 {
-    public class OpenTelemetryClient
+    public class OpenTelemetryClient : ILogger
     {
         private ILogger logger;
 
@@ -13,25 +14,41 @@ namespace Microsoft.CloudMine.Core.Telemetry
         {
             ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddOpenTelemetry(loggerOptions =>
             {
-                loggerOptions.AddGenevaLogExporter(exporterOptions =>
-                {
-                    exporterOptions.ConnectionString = "EtwSession=OpenTelemetry";
+                loggerOptions.AddConsoleExporter();
+                //loggerOptions.AddGenevaLogExporter(exporterOptions =>
+                //{
+                //    exporterOptions.ConnectionString = "EtwSession=OpenTelemetry";
 
-                    exporterOptions.PrepopulatedFields = new Dictionary<string, object>
-                    {
-                        ["cloud.role"] = "BusyWorker",
-                        ["cloud.roleInstance"] = "CY1SCH030021417",
-                        ["cloud.roleVer"] = "9.0.15289.2",
-                    };
-                });
+                //    exporterOptions.PrepopulatedFields = new Dictionary<string, object>
+                //    {
+                //        ["cloud.role"] = "BusyWorker",
+                //        ["cloud.roleInstance"] = "CY1SCH030021417",
+                //        ["cloud.roleVer"] = "9.0.15289.2",
+                //    };
+                //});
             }));
 
             this.logger = loggerFactory.CreateLogger<OpenTelemetryClient>();
         }
 
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return logger.BeginScope(state);
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logger.IsEnabled(logLevel);
+        }
+
         public void Log(string message)
         {
             this.logger.LogInformation(message);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logger.Log(logLevel, eventId, state, exception, formatter);
         }
     }
 }
