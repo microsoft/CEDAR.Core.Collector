a2d96839a922c451238ac5ae9b7f5b7dd113a1f0
luke gostling <lukegostling@microsoft.com>
2022-04-21 10:25:05 -0700

    modified OPenTelemetryClient

--- Core.Telemetry/OpenTelemetryClient.cs
+++ Core.Telemetry/OpenTelemetryClient.cs
@@ -1,28 +1,39 @@
 ﻿using Microsoft.Extensions.Logging;
-using OpenTelemetry.Logs;
-using OpenTelemetry.Exporter.Geneva;
 using System.Collections.Generic;
 using System;
+using System.Diagnostics.Metrics;
+using OpenTelemetry;
+using OpenTelemetry.Exporter.Geneva;
+using OpenTelemetry.Metrics;
+using System.Diagnostics;
 
 namespace Microsoft.CloudMine.Core.Telemetry
 {
-    public class OpenTelemetryClient : ILogger
+    public class OpenTelemetryClient
     {
         private ILogger logger;
+        private MeterProvider meterProvider;
+        private static readonly Meter MyMeter = new Meter("Microsoft.CloudMine.Collectors", "1.0");
+        private static readonly Counter<long> MyHeartbeatCounter = MyMeter.CreateCounter<long>("MyHeartbeatCounter");
 
         public OpenTelemetryClient()
+        {
+            this.BuildLogger();
+            this.BuildMetricsExporter();
+        }
+
+        private void BuildLogger()
         {
             ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddOpenTelemetry(loggerOptions =>
             {
                 loggerOptions.AddGenevaLogExporter(exporterOptions =>
                 {
                     exporterOptions.ConnectionString = "EtwSession=OpenTelemetry";
-
                     exporterOptions.PrepopulatedFields = new Dictionary<string, object>
                     {
-                        ["cloud.role"] = "BusyWorker",
-                        ["cloud.roleInstance"] = "CY1SCH030021417",
-                        ["cloud.roleVer"] = "9.0.15289.2",
+                        //["cloud.role"] = "Collectors",
+                        //["cloud.roleInstance"] = "CY1SCH030021417",
+                        //["cloud.roleVer"] = "9.0.15289.2",
                     };
                 });
             }));
@@ -30,14 +41,9 @@ namespace Microsoft.CloudMine.Core.Telemetry
             this.logger = loggerFactory.CreateLogger<OpenTelemetryClient>();
         }
 
-        public IDisposable BeginScope<TState>(TState state)
+        private void BuildMetricsExporter()
         {
-            return logger.BeginScope(state);
-        }
-
-        public bool IsEnabled(LogLevel logLevel)
-        {
-            return logger.IsEnabled(logLevel);
+            
         }
 
         public void Log(string message)
@@ -45,9 +51,20 @@ namespace Microsoft.CloudMine.Core.Telemetry
             this.logger.LogInformation(message);
         }
 
-        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        public void EmitMetric(long num)
         {
-            logger.Log(logLevel, eventId, state, exception, formatter);
+            this.meterProvider = Sdk.CreateMeterProviderBuilder()
+            .AddMeter("Microsoft.CloudMine.*")
+            .AddGenevaMetricExporter(options =>
+            {
+                options.ConnectionString = "Account=CloudMine;Namespace=CmCollectors";
+            })
+            .Build();
+
+            TagList tags = new TagList();
+            tags.Add("text", "hello world");
+            MyHeartbeatCounter.Add(num, tags);
         }
+
     }
 }
