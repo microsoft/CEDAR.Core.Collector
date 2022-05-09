216f822d02220ea107aaebac15a56e8ce0bfc052
Luke Gostling <37627478+lgostling@users.noreply.github.com>
2022-05-09 12:12:33 -0700

    Add support for OpenTelemetry to Core.Collectors (#118)
    ## Description
    Adds base code for logging and auditing using the OpenTelemetry library (exported to Geneva or console).
    OpenTelemetry provides logs, metrics, traces, and audit-logs for monitoring our production function apps. 
    
    ### In production environments OT will export to the following locations:
    
    Logs -> Geneva DGrep `Log` table
    Traces -> Geneva DGrep `Span` table
    Metrics -> Geneva metrics
    AuditLogs -> Geneva DGrep `AsmAuditDP` table
    
    ### In any other environment OT will export to the following locations:
    Logs -> Console
    Traces -> Console
    Metrics -> Console
    Audit Logs -> NONE ()
    
    ## Code Changes
    1. New classes
    -  `OpenTelemetryLoggerProvider` - serves as a logger factory used to inject OpenTelemetry logger into function apps. responsible for disposing all OpenTelemetry objects.
    - `OpenTelemetryMetric` - provides static objects for OT metrics, and individual constructed metrics (should be extended in ADO + GitHub to provide unique metrics
    - `OpenTelemetryTracer` - provides static objects for OT tracing
    - `OpenTelemetryTrace` - provides enum of trace types. should be extended in ADO + GitHub.
    - `OpenTelemetryHelpers` - loads environment variables for OT and provides extensions to `Activity` and `Counter` class for ease of use.
    2. Refactor `IAuditLogger` + `IfxAuditLogger` to use OpenTelemetry IFx logs (IfxAuditLogger renamed to OpenTelemetryAuditLogger)
    3. Modify `AplicationInsightsTelemetryClient` to make logs emitted here readable.
    4. Add a RecordCount metric to `StatsTracker`
    5. Add trace to collection node processing in `CollectorBase`
    6. Add trace to requests in `HttpClientWrapper`
    
    ## Notes
    - Geneva IFX logs do not allow us to cut only the first 4 characters off an ApplicationID, expects a full GUID.
    - Geneva IFX logs do not allow empty, null, or whitespace resource names, we do this when making get requests. (empty request body)

---
+++ Core.Telemetry/OpenTelemetryHelpers.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace Microsoft.CloudMine.Core.Telemetry
+{
+    public static class OpenTelemetryHelpers
+    {
+        public const string GenevaExporterName = "Geneva";
+
+        public static readonly string Product;
+        public static readonly string Service;
+        public static readonly string AppEnvironment;
+        public static readonly string Deployment;
+        public static readonly string Region;
+        public static readonly string OpenTelemetryExporter;
+        
+        static OpenTelemetryHelpers()
+        {
+            OpenTelemetryExporter = Environment.GetEnvironmentVariable("OpenTelemetryExporter") ?? "*";
+            AppEnvironment = Environment.GetEnvironmentVariable("Environment") ?? "*";
+            Deployment = Environment.GetEnvironmentVariable("Deployment") ?? "*";
+            Region = Environment.GetEnvironmentVariable("Region") ?? "*";
+            Product = Environment.GetEnvironmentVariable("Product");
+            Service = Environment.GetEnvironmentVariable("Service");
+
+            if( Product == null || Service == null )
+            {
+                OpenTelemetryExporter = "*";
+                Product = "*";
+                Service = "Collectors";
+            }
+        }
+
+        public static Activity AddDefaultTags(this Activity activity)
+        {
+            activity.AddTag("Product", Product);
+            activity.AddTag("Service", Service);
+            activity.AddTag("Environment", AppEnvironment);
+            activity.AddTag("Deployment", Deployment);
+            activity.AddTag("Region", Region);
+            activity.AddTag("Depth", GetActivityDepth() + 1);
+            return activity;
+        }
+
+        public static int GetActivityDepth()
+        { 
+            if (Activity.Current?.GetTagItem("Depth") is int depth)
+            {
+                return depth;
+            }
+
+            return -1;
+        }
+
+        public static void AddWithDefaultTags<T>(this Counter<T> counter, T value, TagList tags) where T : struct
+        {
+            tags.Add("Product", Product);
+            tags.Add("Service", Service);
+            tags.Add("Environment", AppEnvironment);
+            tags.Add("Deployment", Deployment);
+            tags.Add("Region", Region);
+            counter.Add(value, tags);
+        }
+    }
+}
