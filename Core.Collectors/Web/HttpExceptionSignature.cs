0b0d29209a8bc8f907e26908601aec1a43fabfa1
Alan Klimowski <alankl@microsoft.com>
2022-05-12 16:06:12 -0700

    Move and unify http exception signatures

--- Core.Collectors/Web/HttpExceptionSignature.cs
+++ Core.Collectors/Web/HttpExceptionSignature.cs
@@ -5,21 +5,47 @@ using Microsoft.CloudMine.Core.Collectors.Collector;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Microsoft.CloudMine.Core.Collectors.Web
 {
     public class HttpExceptionSignature : IAllowListStatus
     {
-        public static readonly HttpExceptionSignature RequestTimeoutException = new HttpExceptionSignature(exception =>
+        public static HttpExceptionSignature RequestTimeoutException(Func<HttpRequestMessage, List<CollectionNode>> continuation = null)
         {
-            Type exceptionType = exception.GetType();
-            return exceptionType == typeof(TaskCanceledException) && exception.Message.Equals("The operation was canceled.");
-        });
+            static bool matcher(Exception exception)
+            {
+                Type exceptionType = exception.GetType();
+                return exceptionType == typeof(TaskCanceledException) && exception.Message.Equals("The operation was canceled.");
+            }
+            return new HttpExceptionSignature(matcher, continuation);
+        }
+
+        public static HttpExceptionSignature SocketClosedException(Func<HttpRequestMessage, List<CollectionNode>> continuation = null)
+        {
+            static bool matcher(Exception exception)
+            {
+                Type exceptionType = exception.GetType();
+                return exceptionType == typeof(SocketException) && exception.Message.Equals("An existing connection was forcibly closed by the remote host.");
+            }
+            return new HttpExceptionSignature(matcher, continuation);
+        }
+
+        public static HttpExceptionSignature FailedToParseResponseException(Func<HttpRequestMessage, List<CollectionNode>> continuation = null)
+        {
+            static bool matcher(Exception exception)
+            {
+                Type exceptionType = exception.GetType();
+                return exceptionType == typeof(JsonReaderException) && exception.Message.StartsWith("Error reading JObject from JsonReader.");
+            }
+            return new HttpExceptionSignature(matcher, continuation);
+        }
 
         private readonly Func<Exception, bool> matcher;
         private readonly Func<HttpRequestMessage, List<CollectionNode>> continuation;
-        public HttpExceptionSignature(Func<Exception, bool> matcher, Func<HttpRequestMessage, List<CollectionNode>> continuation = null)
+        protected HttpExceptionSignature(Func<Exception, bool> matcher, Func<HttpRequestMessage, List<CollectionNode>> continuation = null)
         {
             this.matcher = matcher;
             this.continuation = continuation;
@@ -36,7 +62,6 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
             {
                 return new List<CollectionNode>();
             }
-
             return this.continuation(failedRequest);
         }
     }
