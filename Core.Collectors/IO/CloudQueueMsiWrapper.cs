8eb26565806a405a0e786853a48379bbd3d67cc4
Varsha Vadaga <varshavadaga@microsoft.com>
2022-06-29 14:58:32 -0700

    odified check for token refresh

--- Core.Collectors/IO/CloudQueueMsiWrapper.cs
+++ Core.Collectors/IO/CloudQueueMsiWrapper.cs
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.CloudMine.Core.Telemetry;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Microsoft.CloudMine.Core.Collectors.IO
@@ -14,14 +16,15 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
         private CloudQueue queue;
         private readonly string queueName;
         private readonly string storageAccountNameEnvironmentVariable;
-        private DateTime lastMsiTokenRefreshDateUtc = DateTime.MinValue;
-        private readonly static TimeSpan MsiTokenRefreshFrequency = TimeSpan.FromHours(23.9167);
+        private readonly ITelemetryClient telemetryClient;
+        private DateTime msiTokenExpiration = DateTime.MinValue;
 
-        public CloudQueueMsiWrapper(CloudQueue queue, string storageAccountNameEnvironmentVariable)
+        public CloudQueueMsiWrapper(CloudQueue queue, string storageAccountNameEnvironmentVariable, ITelemetryClient telemetryClient)
         {
             this.queue = queue;
             this.queueName = queue.Name;
             this.storageAccountNameEnvironmentVariable = storageAccountNameEnvironmentVariable;
+            this.telemetryClient = telemetryClient;
         }
 
         public Task PutObjectAsJsonStringAsync(object obj)
@@ -49,14 +52,19 @@ namespace Microsoft.CloudMine.Core.Collectors.IO
         }
 
         private async Task<CloudQueue> GetValidMsiStorageQueueAsync()
-
         {
-            TimeSpan elapsed = DateTime.UtcNow - this.lastMsiTokenRefreshDateUtc;
-            //If the elapsed time >= 23 hours 55 mins, then fetch the queue with refreshed token. Tokens acquired via the App Authentication library currently are refreshed when less than 5 minutes remains until they expire. So it caches the token for 23 hours 55 minutes in memory.
-            if (elapsed >= MsiTokenRefreshFrequency)
+            string queueName = this.queue.Name;
+            // Tokens acquired via the App Authentication library currently are refreshed when less than 5 minutes remains until they expire. So it caches the token for 23 hours 55 minutes in memory.
+            if (this.msiTokenExpiration - DateTime.UtcNow <= TimeSpan.FromMinutes(5))
             {
-                this.queue = await AzureHelpers.GetStorageQueueUsingMsiAsync(this.queueName, storageAccountNameEnvironmentVariable).ConfigureAwait(false);
-                this.lastMsiTokenRefreshDateUtc = DateTime.UtcNow;
+                this.queue = await AzureHelpers.GetStorageQueueUsingMsiAsync(queueName, storageAccountNameEnvironmentVariable, telemetryClient).ConfigureAwait(false);
+                this.msiTokenExpiration = AzureHelpers.TokenExpiration;
+                Dictionary<string, string> properties = new Dictionary<string, string>()
+                    {
+                        { "MsiTokenExpirationTime", this.msiTokenExpiration.ToString() },
+                        { "Queue", queueName },
+                    };
+                telemetryClient.TrackEvent("GCRefreshedMsiToken", properties);
             }
             return this.queue;
         }
