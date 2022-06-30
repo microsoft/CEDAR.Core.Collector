a93e23c5ebc62907578a2b43837ea63fd5715e3e
Varsha Vadaga <varshavadaga@microsoft.com>
2022-06-29 17:45:04 -0700

    Core changes for Managed Identities in Github collectors (#147)
    * added managed service identity in core IO
    
    * changes to replace conn strings with managed identity for storage account
    
    * trying to overcome anonymous read
    
    * cleanup after adding required role assignment on storage account
    
    * removed notification queue conn string as it belongs to the same storage account
    
    * Revert "removed notification queue conn string as it belongs to the same storage account"
    
    This reverts commit 8e9c11754ed8f31bb36c1dde2fe08180b61cefd3.
    
    * code review changes
    
    * NRE check
    
    * code review changes
    
    * changes to fix AzureServiceTokenProviderEception
    
    * replaced connection string token with storage account token for record writer
    
    * changes to ensure ROS doesn't use MSI
    
    * change to helper methods to avoid having ros and routers release
    
    * reverted changes to undo changes on ROS, Routers
    
    * missed blob writer tokens earlier
    
    * changes to succeed ado-core validation
    
    * removed the blob content helper that uses conn string
    
    * removed duplicate msi helper
    
    * added duplicate to succeed checks
    
    * test commit
    
    * reverted changes to be alike with main
    
    * modified wrapper classes for queues fetched via msi
    
    * elapsed time changes for refresh token
    
    * odified check for token refresh
    
    * nit fixes
    
    * nit

---
+++ Core.Collectors/IO/CloudQueueMsiWrapper.cs
@@ -0,0 +1,72 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.CloudMine.Core.Telemetry;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.CloudMine.Core.Collectors.IO
+{
+    public class CloudQueueMsiWrapper : IQueue
+    {
+        private CloudQueue queue;
+        private readonly string queueName;
+        private readonly string storageAccountNameEnvironmentVariable;
+        private readonly ITelemetryClient telemetryClient;
+        private DateTime msiTokenExpiration = DateTime.MinValue;
+
+        public CloudQueueMsiWrapper(CloudQueue queue, string storageAccountNameEnvironmentVariable, ITelemetryClient telemetryClient)
+        {
+            this.queue = queue;
+            this.queueName = queue.Name;
+            this.storageAccountNameEnvironmentVariable = storageAccountNameEnvironmentVariable;
+            this.telemetryClient = telemetryClient;
+        }
+
+        public Task PutObjectAsJsonStringAsync(object obj)
+        {
+            string message = JsonConvert.SerializeObject(obj);
+            return this.PutMessageAsync(message);
+        }
+
+        public async Task PutMessageAsync(string message)
+        {
+            CloudQueue queue = await GetValidMsiStorageQueueAsync().ConfigureAwait(false);
+            await queue.AddMessageAsync(new CloudQueueMessage(message)).ConfigureAwait(false);
+        }
+
+        public async Task PutMessageAsync(string message, TimeSpan timeToLive)
+        {
+            CloudQueue queue = await GetValidMsiStorageQueueAsync().ConfigureAwait(false);
+            await queue.AddMessageAsync(new CloudQueueMessage(message), timeToLive, null, new QueueRequestOptions(), new OperationContext());
+        }
+
+        public Task PutObjectAsJsonStringAsync(object obj, TimeSpan timeToLive)
+        {
+            string message = JsonConvert.SerializeObject(obj);
+            return this.PutMessageAsync(message, timeToLive);
+        }
+
+        private async Task<CloudQueue> GetValidMsiStorageQueueAsync()
+        {
+            string queueName = this.queue.Name;
+            // Tokens acquired via the App Authentication library currently are refreshed when less than 5 minutes remains until they expire. So it caches the token for 23 hours 55 minutes in memory.
+            if (this.msiTokenExpiration - DateTime.UtcNow <= TimeSpan.FromMinutes(5))
+            {
+                this.queue = await AzureHelpers.GetStorageQueueUsingMsiAsync(queueName, this.storageAccountNameEnvironmentVariable, this.telemetryClient).ConfigureAwait(false);
+                this.msiTokenExpiration = AzureHelpers.TokenExpiration;
+                Dictionary<string, string> properties = new Dictionary<string, string>()
+                {
+                    { "MsiTokenExpirationTime", this.msiTokenExpiration.ToString() },
+                    { "Queue", queueName },
+                };
+                telemetryClient.TrackEvent("RefreshedMsiToken", properties);
+            }
+            return this.queue;
+        }
+    }
+}
