9abc177bdb18f8bfe5f3f43694fea54fbc12c4c9
Kivanc Muslu <43969379+kivancmuslu@users.noreply.github.com>
2022-09-12 10:23:13 -0700

    Filter large responses (#167)
    * Filter large responses

--- Pipelines/Core/Collectors/Core.Collectors/Web/HttpUtility.cs
+++ Pipelines/Core/Collectors/Core.Collectors/Web/HttpUtility.cs
@@ -1,8 +1,9 @@
-﻿// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,6 +18,20 @@ namespace Microsoft.CloudMine.Core.Collectors.Web
             using StreamReader streamReader = new StreamReader(responseStream);
             using JsonTextReader jsonTextReader = new JsonTextReader(streamReader);
             return JObject.Load(jsonTextReader);
+        }
+
+        public static async Task<Tuple<JObject, long>> TryParseAsJObjectAsync(HttpResponseMessage response, long maxResponseLength)
+        {
+            using Stream responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+            long responseLength = responseStream.Length;
+            if (responseLength > maxResponseLength)
+            {
+                return Tuple.Create((JObject)null, responseLength);
+            }
+
+            using StreamReader streamReader = new StreamReader(responseStream);
+            using JsonTextReader jsonTextReader = new JsonTextReader(streamReader);
+            return Tuple.Create(JObject.Load(jsonTextReader), responseLength);
         }
 
         public static async Task<JArray> ParseAsJArrayAsync(HttpResponseMessage response)
